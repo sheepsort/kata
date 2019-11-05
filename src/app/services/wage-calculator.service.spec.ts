f7f9150e95fdfc77758545f16bec375e97c13dcc
Jim <jim@sheepsort.com>
2019-11-04 21:35:14 -0600

    Starting on the wage calculating service

---
+++ src/app/services/wage-calculator.service.spec.ts
@@ -0,0 +1,27 @@
+import { WageCalculatorService } from './wage-calculator.service';
+
+describe('WageCalculatorService', () => {
+  let service: WageCalculatorService;
+
+  beforeEach(() => {
+    service = new WageCalculatorService();
+  });
+
+  // These dummy tests are auto-populated by Angular and tend to be most useful for ensuring the test's beforeEach is properly setup.
+  // I use them often in refactoring old test code that may have error-suppressing schemas or mocked services,
+  // many of which ought to include at least one full integration-style test (but don't).
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should compute 80 dollars when ten earned hours are worked', () => {
+    // arrange
+    const earnedHours = 10;
+
+    // act
+    const wages = service.CalculateWages(earnedHours);
+
+    // assert
+    expect(wages).toBe(80);
+  })
+});
