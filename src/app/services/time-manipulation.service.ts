893c74a50e00f18d934b7ab10b5d11cb6f2f6c32
Jim <jim@sheepsort.com>
2019-11-04 22:02:28 -0600

    add method to beautify time

--- src/app/services/time-manipulation.service.ts
+++ src/app/services/time-manipulation.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class TimeManipulationService {
 
   constructor() { }
+
+  OnMakeFriendlyTime(time: number): string {
+    if (time > 12) {
+      time -= 12;
+      return `${time}:00PM`;
+    } else if (time > 0) {
+      return `${time}:00AM`;
+    } else {
+      return `12:00AM`;
+    }
+  }
 }
