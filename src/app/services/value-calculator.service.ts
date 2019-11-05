7ccdd1ef31b03dd0a5c52ce66a092ec439feca43
Jim <jim@sheepsort.com>
2019-11-05 10:01:16 -0600

    finishing the service calculation

--- src/app/services/value-calculator.service.ts
+++ src/app/services/value-calculator.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Constants } from '../helpers/constants.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValueCalculatorService {
+  base = new Constants().BASE;
 
   constructor() { }
 
   OnCalculateValue(hourlyWage: number): number {
-
+    return hourlyWage / this.base;
   }
 }
